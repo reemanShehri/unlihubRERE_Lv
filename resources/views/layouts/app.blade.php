10593077a9f8f3dfacd2b037b727832b752f03a2
reemanShehri <https://github.com/reemanShehri>
2025-06-22 16:42:10 +0300

    ti

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        <script src="https://cdn.tailwindcss.com"></script>
+    <title>{{ isset($title) ? $title : 'UniHub' }}</title>        <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
